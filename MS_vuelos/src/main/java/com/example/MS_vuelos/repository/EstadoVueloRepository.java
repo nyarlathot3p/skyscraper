0561836a0144f0aba327b7790e020f8d96d569d7
Notzeiro <hectorzutreras@gmail.com>
2026-05-19 01:51:55 -0400

    chore: isolate MS_reservas

--- MS_vuelos/src/main/java/com/example/MS_vuelos/repository/EstadoVueloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MS_vuelos.repository;
-
-import com.example.MS_vuelos.model.entity.EstadoVuelo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstadoVueloRepository extends JpaRepository<EstadoVuelo, Long> {
-}
